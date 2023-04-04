3e64b3bdb7066e0b3b8481ac51920e062225242f
Frédéric Devernay <devernay@users.noreply.github.com>
2023-04-04 10:49:59 -0700

    OCIOPluginBase.h: add license block

--- OCIO/OCIOPluginBase.h
+++ OCIO/OCIOPluginBase.h
@@ -1,3 +1,22 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * This file is part of openfx-io <https://github.com/NatronGitHub/openfx-io>,
+ * (C) 2018-2023 The Natron Developers
+ * (C) 2013-2018 INRIA
+ *
+ * openfx-io is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * openfx-io is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with openfx-io.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>
+ * ***** END LICENSE BLOCK ***** */
+
 #ifndef IO_OCIOPluginBase_h
 #define IO_OCIOPluginBase_h
 
