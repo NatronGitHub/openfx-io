NatronGitHub/openfx-io

commit 6d6eda2c0391fb702c5173bc7204966672c56c0a

WriteOIIO: quality is now part of compression (since OIIO 2.3.16) (#31)
* WriteOIIO: quality is now part of compression (since OIIO 2.3.16)

fixes https://github.com/NatronGitHub/Natron/issues/889

OIIO/WriteOIIO.cpp
@@ -806,22 +806,30 @@ WriteOIIOPlugin::refreshParamsVisibility(const string& filename)
     if (output.get()) {
         _tileSize->setIsSecretAndDisabled(!output->supports("tiles"));
         //_outputLayers->setIsSecretAndDisabled(!output->supports("nchannels"));
-        bool hasQuality = (strcmp(output->format_name(), "jpeg") == 0 || strcmp(output->format_name(), "webp") == 0);
-        if (!hasQuality && (strcmp(output->format_name(), "tiff") == 0)) {
-            int compression_i;
-            _compression->getValue(compression_i);
-            hasQuality = ((EParamCompression)compression_i == eParamCompressionJPEG);
-        }
-        _quality->setIsSecretAndDisabled(!hasQuality);
+
+        // hasQuality: search for uses of decode_compression_metadata() in OIIO source code.
+        // output->supports("quality") still returns false for all formats, but may
+        // be implemented someday, see https://github.com/OpenImageIO/oiio/issues/3859
+        bool hasQuality = (strcmp(output->format_name(), "jpeg") == 0 ||
+                           strcmp(output->format_name(), "webp") == 0 ||
+                           strcmp(output->format_name(), "heic") == 0 ||
+                           strcmp(output->format_name(), "avif") == 0 ||
+                           output->supports("quality"));
+        bool hasDWA = false;
         bool isEXR = strcmp(output->format_name(), "openexr") == 0;
-        bool hasDWA = isEXR;
-        if (hasDWA) {
+        bool isTIFF = strcmp(output->format_name(), "tiff") == 0;
+        if (isEXR || isTIFF) {
             int compression_i;
             _compression->getValue(compression_i);
             EParamCompression compression = (EParamCompression)compression_i;
-            hasDWA = (compression == eParamCompressionDWAa) || (compression == eParamCompressionDWAb);
+            hasQuality = (compression == eParamCompressionJPEG ||
+                          compression == eParamCompressionZip);
+            if (isEXR) {
+                hasDWA = (compression == eParamCompressionDWAa) || (compression == eParamCompressionDWAb);
+            }
         }
         _dwaCompressionLevel->setIsSecretAndDisabled(!hasDWA);
+        _quality->setIsSecretAndDisabled(!hasQuality);
         if (_views) {
             _views->setIsSecretAndDisabled(!isEXR);
         }
@@ -1099,10 +1107,18 @@ WriteOIIOPlugin::beginEncodeParts(void* user_data,
     }
 #endif // ifdef OFX_IO_USING_OCIO
     if (!_quality->getIsSecret()) {
+#if OIIO_VERSION >= 20100 // Introduced in OIIO 2.1.0 https://github.com/OpenImageIO/oiio/pull/2111
+        compression += ':' + std::to_string(quality);
+#else
         spec.attribute("CompressionQuality", quality);
+#endif
     }
     if (!_dwaCompressionLevel->getIsSecret()) {
+#if OIIO_VERSION >= 20100 // Introduced in OIIO 2.1.0 https://github.com/OpenImageIO/oiio/pull/2111
+        compression += ':' + std::to_string((int)dwaCompressionLevel);
+#else
         spec.attribute("openexr:dwaCompressionLevel", (float)dwaCompressionLevel);
+#endif
     }
     spec.attribute("Orientation", orientation + 1);
     if (!compression.empty()) { // some formats have a good value for the default compression

commit af7dd7856516febf02f539ffbef19a90f887154c

Fix compression quality for image file formats like jpeg.
This fixes an issue introduced by https://github.com/NatronGitHub/openfx-io/pull/31
that causes the compression quality to be ignored for formats with
default compression like JPEG files. In the case of these files,
the compression string was not being set and so the line that was
appending the quality was actually triggering behavior that caused
the quality value to be ignored. This in turn started causing the
Natron-Tests to start failing because the JPEG writer used in these
tests was not honoring the requested quality value.

The fix here is simply to specify a valid base compression string
for these formats so that when the quality is appended, the
compression/codec part of the string is not empty. This avoids
triggering logic in OIIO that ignores the quality values for
empty compression/codec strings.

OIIO/WriteOIIO.cpp
@@ -1015,8 +1015,16 @@ WriteOIIOPlugin::beginEncodeParts(void* user_data,
     string compression;
 
     switch ((EParamCompression)compression_i) {
-    case eParamCompressionAuto:
-        break;
+    case eParamCompressionAuto: {
+        // Set compression string for formats that only have a single compression type.
+        static const char* formats[] = {"jpeg", "webp", "heic", "avif", nullptr};
+        for (int i = 0; formats[i] != nullptr; ++i) {
+            if (strcmp(data->output->format_name(), formats[i]) == 0) {
+                compression = formats[i];
+                break;
+            }
+        }
+    } break;
     case eParamCompressionNone: // EXR, TIFF, IFF
         compression = "none";
         break;